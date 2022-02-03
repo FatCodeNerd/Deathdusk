40004834e48ca3cb15750f3c7b97ae26a5bb3366
TheButterbrotMan <64548098+TheButterbrotMan@users.noreply.github.com>
2022-02-03 03:12:27 +0100

    DEV

---
+++ ChangelogGenerator.bat
@@ -0,0 +1,1 @@
+java -jar ChangelogGenerator.jar -m -n manifest.json -o C:/Users/elias/AppData/Roaming/gdlauncher_next/instances/Deathdusk/manifest.json
---
+++ kubejs/startup_scripts/remove.js
@@ -0,0 +1,76 @@
+onEvent('recipes', event => {
+
+// Hyper Lighting [Fabric]
+event.remove({id:'hyperlighting:wireless_powercard'})
+event.remove({id:'hyperlighting:wireless_switch_card'})
+
+// Spectrum
+event.remove({id:'spectrum:redstone_wireless'})
+
+// Tom's Simple Storage Mod (Fabric)
+event.remove({id:'toms_storage:ts.wireless_terminal'})
+event.remove({id:'toms_storage:ts.adv_wireless_terminal'})
+
+// Golems Galore
+event.remove({id:'golemsgalore:obamium_block'})
+event.remove({id:'golemsgalore:obamium_ingot'})
+
+// Taverns (Fabric)
+event.remove({id:'taverns:silvercoin'})
+event.remove({id:'taverns:halfgoldcoin'})
+event.remove({id:'taverns:halfsilvercoin'})
+event.remove({id:'taverns:coppercoin'})
+event.remove({id:'taverns:netheritecoin'})
+event.remove({id:'taverns:halfcoppercoin'})
+event.remove({id:'taverns:goldcoin'})
+event.remove({id:'taverns:halfnetheritecoin'})
+event.remove({id:'taverns:rawnetheritecoin'})
+event.remove({id:'taverns:rawhalfcoppercoin'})
+event.remove({id:'taverns:rawgoldcoin'})
+event.remove({id:'taverns:rawhalfgoldcoin'})
+event.remove({id:'taverns:rawsilvercoin'})
+event.remove({id:'taverns:rawhalfsilvercoin'})
+event.remove({id:'taverns:rawhalfgoldcoin'})
+event.remove({id:'taverns:rawcoppercoin'})
+event.remove({id:'taverns:rawhalfnetheritecoin'})
+
+// Immortuos Calyx
+event.remove({id:'immortuoscalyx:healthscanner'})
+event.remove({id:'immortuoscalyx:infection_scanner'})
+
+// MCDoom
+event.remove({id:'doom:gun_table'})
+event.remove({id:'doom:gas_barrel'})
+event.remove({id:'doom:chaingunbullets'})
+event.remove({id:'doom:rocketlauncher'})
+event.remove({id:'doom:chainsaweternal'})
+event.remove({id:'doom:doomed_plasma_rifle'})
+event.remove({id:'doom:shotgun_shells'})
+event.remove({id:'doom:energy_cells'})
+event.remove({id:'doom:doomed_grenade'})
+event.remove({id:'doom:supershotgun'})
+event.remove({id:'doom:plasmagun'})
+event.remove({id:'doom:chainsaw64'})
+event.remove({id:'doom:bfg_cell'})
+event.remove({id:'doom:bullets'})
+event.remove({id:'doom:doomed_shotgun'})
+event.remove({id:'doom:pistol'})
+event.remove({id:'doom:chainsaw'})
+event.remove({id:'doom:chaingun'})
+event.remove({id:'doom:doomed_gauss'})
+event.remove({id:'doom:unmaykr'})
+event.remove({id:'doom:rocket'})
+event.remove({id:'bfg_eternal'})
+event.remove({id:'doom:bfg9000'})
+event.remove({id:'doom:shotgun'})
+event.remove({id:'doom:jump_pad'})
+
+// The End Expansion By Igoriusss
+event.remove({id:'tee:ties'})
+
+// SurvivalPlus
+event.remove({id:'survivalplus:stone_leggings'})
+event.remove({id:'survivalplus:stone_boots'})
+event.remove({id:'survivalplus:stone_chestplate'})
+event.remove({id:'survivalplus:stone_helmet'})
+})
